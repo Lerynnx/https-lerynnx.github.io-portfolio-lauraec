5d8dff827babdbd8077edf5c810d91483fe40120
LauraPC <lauraescuderocabria@gmail.com>
2026-06-09 16:34:14 +0200

    ci(github_actions): Astro configuration prepared for GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Lerynnx.github.io',
+  base: '/portfolio-lauraec',
   vite: {
     plugins: [tailwindcss()]
   }
