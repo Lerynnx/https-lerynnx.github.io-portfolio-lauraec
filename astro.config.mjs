2eba12c38b73101cf4396ca506e1a1171e5f9340
LauraPC <lauraescuderocabria@gmail.com>
2026-06-09 23:15:12 +0200

    fix(config): remove outdated site URL from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Lerynnx.github.io',
   base: '/portfolio-lauraec',
   vite: {
     plugins: [tailwindcss()]
